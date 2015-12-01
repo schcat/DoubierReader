823057bc5f20e6128d8c10505874cfe40d214b3a
Zhixue Li <schcat@zhixues-macbook-pro.local>
2015-12-01 22:15:57 +0800

    Initial Commit

---
+++ DoubierReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DoubierReader
+//
+//  Created by Zhixue Li on 12/1/15.
+//  Copyright © 2015 Zhixue Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DoubierReader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DoubierReader
+//
+//  Created by Zhixue Li on 12/1/15.
+//  Copyright © 2015 Zhixue Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
